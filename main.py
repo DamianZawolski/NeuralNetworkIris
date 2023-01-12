93ae8efe89e61ede213427b19b7681edb46558f3
DamianZawolski <260353@student.pwr.edu.pl>
2023-01-12 03:18:00 +0100

    v1

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from fpdf import FPDF
+from PIL import Image
 
 
 class SiecNeuronowa:
-    def __init__(self, uczace, liczba_neuronow_w_warstwie_ukrytej=4):
-        np.random.seed(11)
+    def __init__(self, uczace, seed_generatora_losowego, liczba_neuronow_w_warstwie_ukrytej=4):
+        np.random.seed(seed_generatora_losowego)
         wejscie = pd.read_csv(uczace)
 
         zbior_uczacy = self.przygotowanie_danych(wejscie)
         liczba_kolumn = len(zbior_uczacy.columns)
         liczba_wierszy = len(zbior_uczacy.index)
-        self.X = zbior_uczacy.iloc[:, 0:(liczba_kolumn - 3)].values.reshape(liczba_wierszy, liczba_kolumn - 3)
+        self.x = zbior_uczacy.iloc[:, 0:(liczba_kolumn - 3)].values.reshape(liczba_wierszy, liczba_kolumn - 3)
         self.y = zbior_uczacy.iloc[:, (liczba_kolumn - 3):].values.reshape(liczba_wierszy, 3)
 
-        liczba_neuronow_w_warstwie_wejsciowej = len(self.X[0])
+        liczba_neuronow_w_warstwie_wejsciowej = len(self.x[0])
         if not isinstance(self.y[0], np.ndarray):
             rozmiar_warstwy_wyjsciowej = 1
         else:
             rozmiar_warstwy_wyjsciowej = len(self.y[0])
 
         # Przypisanie losowych wag
-        self.wagi_wejsciowa_do_ukrytej = 2 * np.random.random(
+        self.w1 = 2 * np.random.random(
             (liczba_neuronow_w_warstwie_wejsciowej, liczba_neuronow_w_warstwie_ukrytej)) - 1
-        self.dane_wejsciowe_do_ukrytej = self.X
-        self.delta_wejsciowa_do_ukrytej = np.zeros(
+        self.x1 = self.x
+        self.delta_x1 = np.zeros(
             (liczba_neuronow_w_warstwie_wejsciowej, liczba_neuronow_w_warstwie_ukrytej))
-        self.wagi_ukryta_do_wyjsciowej = 2 * np.random.random(
+        self.w2 = 2 * np.random.random(
             (liczba_neuronow_w_warstwie_ukrytej, rozmiar_warstwy_wyjsciowej)) - 1
-        self.dane_ukryta_do_wyjsciowej = np.zeros((len(self.X), liczba_neuronow_w_warstwie_ukrytej))
-        self.delta_ukryta_do_wyjsciowej = np.zeros((liczba_neuronow_w_warstwie_ukrytej, rozmiar_warstwy_wyjsciowej))
-        self.delta_wyjscie = np.zeros((rozmiar_warstwy_wyjsciowej, 1))
+        self.v = np.zeros((len(self.x), liczba_neuronow_w_warstwie_ukrytej))
+        self.delta_v = np.zeros((liczba_neuronow_w_warstwie_ukrytej, rozmiar_warstwy_wyjsciowej))
+        self.delta_y = np.zeros((rozmiar_warstwy_wyjsciowej, 1))
 
-    def sigmoid(self, x):
+    def sigmoid(self, s):
         """Funkcja licząca funkcję sigmoidalną"""
-        return 1 / (1 + np.exp(-x))
+        return 1 / (1 + np.exp(-s))
 
-    def pochodna_sigmoid(self, x):
-        """Pochodna funkcji sigmoidalnej- indykuje pewnośc wobec obecnej wagi"""
-        return x * (1 - x)
+    def pochodna_sigmoid(self, s):
+        """Pochodna funkcji sigmoidalnej- wskazuje pewnośc wobec obecnej wagi"""
+        return s * (1 - s)
 
     def przygotowanie_danych(self, dane):
 
         # 3 neurony wyjściowe ponieważ mamy 3 gatunki kwiatów
-        dane["Y1"] = (dane["Gatunek"] == "Iris-setosa") * 1
-        dane["Y2"] = (dane["Gatunek"] == "Iris-versicolor") * 1
-        dane["Y3"] = (dane["Gatunek"] == "Iris-virginica") * 1
+        dane["J1"] = (dane["Gatunek"] == "Iris-setosa") * 1
+        dane["J2"] = (dane["Gatunek"] == "Iris-versicolor") * 1
+        dane["J3"] = (dane["Gatunek"] == "Iris-virginica") * 1
         dane = dane.drop("Gatunek", 1)  # Usuwanie kolumny z gatunkami
         # Standaryzacja/ normalizacja
         for kolumna in dane.columns[0:4]:
@@ -55,30 +57,31 @@ class SiecNeuronowa:
         return dane
 
     # Trenowanie
-    def trenuj(self, liczba_iteracji, wspolczynnik_uczenia=0.1):
-        blad = 0
+    def uczenie(self, liczba_iteracji, wspolczynnik_uczenia=0.1):
+        e = 0
         for _ in range(liczba_iteracji):
-            wyjscie = self.przekanazanie_danych_do_nastepnej_warstwy()
-            blad = 0.5 * np.power((wyjscie - self.y), 2)
-            self.propagacja_wsteczna(wyjscie)
-            aktualizacja_warstwy_ukrytej = wspolczynnik_uczenia * self.dane_ukryta_do_wyjsciowej.T.dot(
-                self.delta_wyjscie)
-            aktualizajca_warstwy_wejsciowej = wspolczynnik_uczenia * self.dane_wejsciowe_do_ukrytej.T.dot(
-                self.delta_ukryta_do_wyjsciowej)
+            d = self.przekanazanie_danych_do_warstwy_wyjsciowej()
+            e = 0.5 * np.power((d - self.y), 2)
+            self.propagacja_wsteczna(d)
+            aktualizacja_warstwy_ukrytej = wspolczynnik_uczenia * self.v.T.dot(
+                self.delta_y)
+            aktualizajca_warstwy_wejsciowej = wspolczynnik_uczenia * self.x1.T.dot(
+                self.delta_v)
 
-            self.wagi_ukryta_do_wyjsciowej += aktualizacja_warstwy_ukrytej
-            self.wagi_wejsciowa_do_ukrytej += aktualizajca_warstwy_wejsciowej
+            self.w2 += aktualizacja_warstwy_ukrytej
+            self.w1 += aktualizajca_warstwy_wejsciowej
 
-        print(f"Błąd po {liczba_iteracji} iteracjach wynosi {np.sum(blad)}")
-        print("Finalne wagi wektorów w wejściowej do ukrytej")
-        print(self.wagi_wejsciowa_do_ukrytej)
-        print("Finalne wagi wektorów w ukrytej do wyjsciowej")
-        print(self.wagi_ukryta_do_wyjsciowej)
+        print(f"Błąd po {liczba_iteracji} iteracjach wynosi {np.sum(e)}")
+        print("Finalne wagi wektorów pomiedzy wejściową a ukrytą")
+        print(self.w1)
+        print("Finalne wagi wektorów pomiedzy ukrytą a wyjściową")
+        print(self.w2)
+        return self.w1, self.w2
 
-    def przekanazanie_danych_do_nastepnej_warstwy(self):
-        wejscie1 = np.dot(self.X, self.wagi_wejsciowa_do_ukrytej)
-        self.dane_ukryta_do_wyjsciowej = self.sigmoid(wejscie1)
-        wejscie2 = np.dot(self.dane_ukryta_do_wyjsciowej, self.wagi_ukryta_do_wyjsciowej)
+    def przekanazanie_danych_do_warstwy_wyjsciowej(self):
+        wejscie1 = np.dot(self.x, self.w1)
+        self.v = self.sigmoid(wejscie1)
+        wejscie2 = np.dot(self.v, self.w2)
         wyjscie = self.sigmoid(wejscie2)
         return wyjscie
 
@@ -87,52 +90,56 @@ class SiecNeuronowa:
         self.delta_warstwy_ukrytej()
 
     def delta_warstwy_wyjsciowej(self, out):
-        self.delta_wyjscie = (self.y - out) * (self.pochodna_sigmoid(out))
+        self.delta_y = (self.y - out) * (self.pochodna_sigmoid(out))
 
     def delta_warstwy_ukrytej(self):
-        self.delta_ukryta_do_wyjsciowej = (self.delta_wyjscie.dot(self.wagi_ukryta_do_wyjsciowej.T)) * (
-            self.pochodna_sigmoid(self.dane_ukryta_do_wyjsciowej))
+        self.delta_v = (self.delta_y.dot(self.w2.T)) * (
+            self.pochodna_sigmoid(self.v))
 
     def delta_warstwy_wejsciowej(self):
-        self.delta_wejsciowa_do_ukrytej = np.multiply(self.pochodna_sigmoid(self.dane_wejsciowe_do_ukrytej),
-                                                      self.delta_wejsciowa_do_ukrytej.dot(
-                                                          self.wagi_wejsciowa_do_ukrytej.T))
+        self.delta_x1 = np.multiply(self.pochodna_sigmoid(self.x1),
+                                    self.delta_x1.dot(
+                                                          self.w1.T))
 
     def predykcja(self, test):
         zbior_testujacy = pd.read_csv(test)
         zbior_testujacy = self.przygotowanie_danych(zbior_testujacy)
         liczba_kolumn = len(zbior_testujacy.columns)
         liczba_wierszy = len(zbior_testujacy.index)
-        self.X = zbior_testujacy.iloc[:, 0:(liczba_kolumn - 3)].values.reshape(liczba_wierszy, liczba_kolumn - 3)
+        self.x = zbior_testujacy.iloc[:, 0:(liczba_kolumn - 3)].values.reshape(liczba_wierszy, liczba_kolumn - 3)
         self.y = zbior_testujacy.iloc[:, (liczba_kolumn - 3):].values.reshape(liczba_wierszy, 3)
-        wyjscie = self.przekanazanie_danych_do_nastepnej_warstwy()
+        d = self.przekanazanie_danych_do_warstwy_wyjsciowej()
         prawidlowe = 0
-        for i in range(len(wyjscie)):
-            if (wyjscie[i][0] > wyjscie[i][1] and wyjscie[i][0] > wyjscie[i][2] and self.y[i][0] == 1) or \
-                    (wyjscie[i][1] > wyjscie[i][0] and wyjscie[i][1] > wyjscie[i][2] and self.y[i][1] == 1) or \
-                    (wyjscie[i][2] > wyjscie[i][0] and wyjscie[i][2] > wyjscie[i][1] and self.y[i][2] == 1):
+        for i in range(len(d)):
+            if (d[i][0] > d[i][1] and d[i][0] > d[i][2] and self.y[i][0] == 1) or \
+                    (d[i][1] > d[i][0] and d[i][1] > d[i][2] and self.y[i][1] == 1) or \
+                    (d[i][2] > d[i][0] and d[i][2] > d[i][1] and self.y[i][2] == 1):
                 prawidlowe += 1
             else:
-                print(f"Błędna klasyfikacja: prawidłowe ({self.y[i]}), uzyskane ({wyjscie[i]})")
-        procent_prawidlowej_klasyfikacji = round(prawidlowe / len(wyjscie) * 100, 2)
+                print(f"Błędna klasyfikacja: prawidłowe ({self.y[i]}), uzyskane ({d[i]})")
+        procent_prawidlowej_klasyfikacji = round(prawidlowe / len(d) * 100, 2)
         print(
-            f"Prawidłowo zidentyfikowano {prawidlowe}/{len(wyjscie)} kwiatów ({procent_prawidlowej_klasyfikacji} procent)")
-        blad_kwadratowy = np.power((wyjscie - self.y), 2)
-        blad_testowania = 0.5 * np.sum(blad_kwadratowy)
-        print(f"\nDokładność wynosi {round(100 - blad_testowania, 2)}%")
-        return blad_testowania, procent_prawidlowej_klasyfikacji
+            f"Prawidłowo zidentyfikowano {prawidlowe}/{len(d)} kwiatów ({procent_prawidlowej_klasyfikacji} procent)")
+
+        "Kwadrat różnicy"
+        epsilon = np.power((d - self.y), 2)
+        "Suma kwadratów"
+        e = 0.5 * np.sum(epsilon)
+        print(f"\nDokładność wynosi {round(100 - e, 2)}%")
+        return e, procent_prawidlowej_klasyfikacji
 
 
-siec_neuronowa = SiecNeuronowa("uczace.csv")
-siec_neuronowa.trenuj(liczba_iteracji=3, wspolczynnik_uczenia=0.3)
-blad_testowy, procent = siec_neuronowa.predykcja("testujace.csv")
-print(f"Błąd testowy w {round(blad_testowy, 2)}% przypadków.")
+seed_generatora_losowego = 11
+siec_neuronowa = SiecNeuronowa("uczace.csv", seed_generatora_losowego)
+wagi_wejsciowa, wagi_ukryta = siec_neuronowa.uczenie(liczba_iteracji=3, wspolczynnik_uczenia=0.3)
+blad_testowy1, procent1 = siec_neuronowa.predykcja("testujace.csv")
+print(f"Błąd testowy w {round(blad_testowy1, 2)}% przypadków.")
 
 lista_bledow = []
 procenty = []
 for i in range(100):
-    siec_neuronowa = SiecNeuronowa("uczace.csv")
-    siec_neuronowa.trenuj(liczba_iteracji=i, wspolczynnik_uczenia=0.3)
+    siec_neuronowa = SiecNeuronowa("uczace.csv", seed_generatora_losowego)
+    siec_neuronowa.uczenie(liczba_iteracji=i, wspolczynnik_uczenia=0.3)
     blad_testowy, procent = siec_neuronowa.predykcja("testujace.csv")
     lista_bledow.append(blad_testowy)
     procenty.append(procent)
@@ -149,7 +156,6 @@ ax.set_title("Błąd testowy w zależności od ilości przejść przez cały zbi
 plt.savefig('Wykres błędów testowych.png')
 plt.show()
 
-
 _, ax = plt.subplots(1)
 ax.plot(procenty, label='Ilość wyrażona w procentach')
 fig = plt.gcf()
@@ -161,3 +167,53 @@ ax.set_xlim(0, 100)
 ax.set_title("Prawidłowo sklasyfikowane kwiaty")
 plt.savefig('Wykres prawidłowych klasyfikacji.png')
 plt.show()
+
+
+class PDF(FPDF):
+    def lines(self):
+        self.rect(5.0, 5.0, 200.0, 287.0)
+
+    def info(self, zdjecie):
+        im = Image.open("irys.jpg")
+        width, height = im.size
+        max_height = 45
+        self.image("irys.jpg", link='', type='', w=width * (max_height / height), h=height * (max_height / height))
+        self.set_xy(0.0, 0.0)
+        self.set_font('Arial', 'B', 26)
+        self.set_text_color(0, 0, 0)
+        self.multi_cell(w=210.0, h=20.0, align='C', txt=f"\nWyniki klasyfikacji irysow", border=0)
+        self.set_font('Arial', 'B', 12)
+        self.multi_cell(w=190.0, h=12.0, align='C', txt=f"(wyniki po trzykrotnym przejsciu przez zbior uczacy)",
+                        border=0)
+        self.multi_cell(w=190.0, h=12.0, align='C',
+                        txt=f"\nWagi pomiedzy warstwa wejsciowa a ukryta \n{wagi_wejsciowa}", border=0)
+        self.multi_cell(w=190.0, h=12.0, align='C', txt=f"\nWagi pomiedzy warstwa ukryta a wyjsciowa \n{wagi_ukryta}",
+                        border=0)
+        self.multi_cell(w=190.0, h=12.0, align='C', txt=f"\nPrawidlowo zidentyfikowano ({procent1} % kwiatow)",
+                        border=0)
+        self.multi_cell(w=190.0, h=12.0, align='C', txt=f"Blad testowy wyniosl {round(blad_testowy1, 2)}% przypadkow.",
+                        border=0)
+        im = Image.open(zdjecie)
+        width, height = im.size
+        max_width = 100
+        height = height * (max_width / width)
+
+        self.set_xy(10.0, 1500)
+        self.set_font('Arial', 'B', 10)
+        self.set_text_color(0, 0, 0)
+        self.multi_cell(w=190.0, h=10.0, align='C', txt=f"Wykres 1", border=0)
+        self.image(f"Wykres błędów testowych.png", link='', type='', w=185, h=105)
+        self.multi_cell(w=190.0, h=10.0, align='C', txt="Wykres 2", border=0)
+        self.image(f"Wykres prawidłowych klasyfikacji.png", link='', type='', w=185, h=105)
+
+
+def create_pdf():
+    pdf = PDF()
+    pdf.add_page()
+    pdf.lines()
+    pdf.info("Wykres prawidłowych klasyfikacji.png")
+    pdf.set_author('Damian Zawolski')
+    pdf.output(f"raport.pdf", 'F')
+
+
+create_pdf()
